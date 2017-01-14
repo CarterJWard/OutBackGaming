80d16760855aeeaa0bc76cb5ca4dafed13cc638e
Nolan <NathanualNolan@live.com>
2017-01-14 15:31:25 +1100

    SQM Build Update
    Just Added some Objects to the Current Map

---
+++ Tanoa Life/Tanoa_Life.Tanoa/Tanoa_Life.Tanoa/config/Config_Prof.hpp
@@ -0,0 +1,29 @@
+/*
+    Proficiency System Config
+*/
+
+class Profs {
+    //CIVILIAN
+    class pCiv {
+        variable = "pCiv";
+        name = "STR_Prof_Civ";
+        tag = "Civilian";
+        side = "civ";
+    };
+    
+    //MILITARY
+    class pCop {
+        variable = "pCop";
+        name = "STR_Prof_Cop";
+        tag = "Military";
+        side = "cop";
+    };
+    
+    //MEDICS
+    class pMed {
+        variable = "pMed";
+        name = "STR_Prof_Med";
+        tag = "Medic";
+        side = "med";
+    };
+};
